2b2f4c29851d20987ee4b5cecfc379c0851d447d
Apo45ty <antonio.tapia@upr.edu>
2018-07-26 16:27:06 -0400

    Added collider component to the level 01 and added the beginings of a health system

--- CameraScroll.cs
+++ CameraScroll.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class CameraScroll : MonoBehaviour {
 
-	public GameObject player;
+	public RPGActor player;
+	public float offscreenDamage = 10;
 
 	void Update () {
 		if (!(player.gameObject.GetComponent<Renderer> ().isVisible)) {
 			//Debug.Log ("Is invisible");
+			player.health -= offscreenDamage;
 			player.transform.position = new Vector2 (transform.position.x, transform.position.y);
 		} else {
 			//Debug.Log ("Is visible");
