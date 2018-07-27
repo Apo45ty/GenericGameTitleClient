d9231f2518d8b6c6990cb6f6b9146bca17bd3481
Apo45ty <antonio.tapia@upr.edu>
2018-07-27 15:56:44 -0400

    Added health bar

---
+++ Health.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+	public RectTransform healthBar;
+	public RPGActor actor;
+	
+	// Update is called once per frame
+	void Update () {
+		healthBar.sizeDelta = new Vector2(
+			actor.health, 
+			healthBar.sizeDelta.y);
+	}
+}
